651ffa2b14c1ebbd32446a9bc651fba68a35c068
Mike Radford <michael.radford@hotmail.co.uk>
2014-12-08 19:41:20 +0000

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -10,7 +10,7 @@ SCCsub <- SCC[c("SCC","Short.Name")]
 ## Plot 1 - 
 plot1 <- aggregate(Emissions ~ year, NEI, sum)
 plot(Emissions ~ year, plot1, type = "b", main="Total US Emissions of PM2.5", xlab="Year", ylab="Emissions (tons)", xaxt="n")
-axis(side=1, at=c("1999", "2002", "2005", "2008"))(
+axis(side=1, at=c("1999", "2002", "2005", "2008"))
 
 dev.copy(png,file="plot1.png")
 dev.off()
