8e9eaf67416c5bb3e31a686cb7e724419f51988d
Mike Radford <michael.radford@hotmail.co.uk>
2014-12-08 19:44:24 +0000

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -2,10 +2,6 @@ library(ggplot2)
 
 ## This first line will likely take a few seconds. Be patient!
 NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
-NEIsub <- NEI[c("fips","SCC","Emissions","type","year")]
-SCCsub <- SCC[c("SCC","Short.Name")]
-
 
 ## Plot 1 - 
 plot1 <- aggregate(Emissions ~ year, NEI, sum)
